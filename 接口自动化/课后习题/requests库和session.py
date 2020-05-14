b69724524d04652e388da56bcb51f11e51fda4e2
Hanyu Wang <wanghanyu1108@outlook.com>
2020-05-14 18:03:06 +0800

    请求头

--- 接口自动化/课后习题/requests库和session.py
+++ 接口自动化/课后习题/requests库和session.py
@@ -1,9 +1,37 @@
 import requests
+import json
 
-# 设置代理
-proxies = {
-    'http': 'http://127.0.0.1:8888',
-    'https': 'http://127.0.0.1:8888',
-}
-response = requests.get('http://mirrors.sohu.com/', proxies=proxies)
-print(response.text)
+# payload = {
+#     "Overall": "良好",
+#     "Progress": "30%",
+#     "Problems": [
+#         {
+#             "No": 1,
+#             "desc": "问题1...."
+#         },
+#         {
+#             "No": 2,
+#             "desc": "问题2...."
+#         },
+#     ]
+# }
+
+# r = requests.post("http://httpbin.org/post", data=json.dumps(payload).encode('utf8'))
+# # r = requests.post("http://httpbin.org/post", json=payload)
+# print(r.text)
+
+# payload = '''
+# <?xml version="1.0" encoding="UTF-8"?>
+# <WorkReport>
+#     <Overall>良好</Overall>
+#     <Progress>30%</Progress>
+#     <Problems>暂无</Problems>
+# </WorkReport>
+# '''
+
+# r = requests.post("http://httpbin.org/post", data=payload.encode('utf8'))
+# print(r.text)
+response = requests.post("http://httpbin.org/post", data={1: 1, 2: 2})
+
+obj = json.loads(response.content.decode('utf8'))
+print(obj)
